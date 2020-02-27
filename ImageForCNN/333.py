6a5185f8f224bba37710304d1fd9fdc71e43d855
Heechul90 <heechul4296@gmail.com>
2020-02-27 14:18:39 +0900

    Image data for preprocessing for CNN

---
+++ ImageForCNN/333.py
@@ -0,0 +1,8 @@
+import mxnet as mx
+import cv2
+from pathlib import Path
+import os
+from time import time
+import shutil
+import matplotlib.pyplot as plt
+
--- ImageForCNN/DogsandCats12.py
+++ ImageForCNN/DogsandCats12.py
@@ -1,24 +1,167 @@
-from __future__ import print_function
+### 개고양이 이미지 분류
+
+# 함수, 모듈 준비
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os, cv2, random
+import gluoncv
 import mxnet as mx
+
 from mxnet import nd, autograd
 from mxnet import gluon
-import numpy as np
-mx.random.seed(1)
+from glob import glob
+from keras.preprocessing import image
 
 ctx = mx.cpu()
+##########################################################
+### seed값 설정
+# seed 값은 random 함수에서 랜덤 값을 계산할 때 사용하며 매 번 바뀝니다.
+# 초기 seed 값을 설정하지 않으면 랜덤 값을 생성하는 순서가 매 번 달라집니다.
+# seed = 0
+# np.random.seed(seed)
+# tf.set_random_seed(seed)
 
-def transformer(data, label):
-    data = mx.image.imresize(data, 224, 224)
-    data = mx.nd.transpose(data, (2, 0, 1))
-    data = data.astype(np.float32)
-    return data, label
+##########################################################
+### dataset 경로 및 사이즈
+ROW, COL = 96, 96
+path = 'D:/HeechulFromGithub/dataset/dogs-vs-cats/train1/'
+
+##########################################################
+### 데이터 불러오기(dog_train)
+# os - 환경 변수나 디렉토리, 파일 등의 OS자원을 제어할 수 있게 해주는 모듈
+# 함수	                설명
+# os.mkdir(디렉터리)	    디렉터리를 생성한다.
+# os.rmdir(디렉터리)	    디렉터리를 삭제한다.단, 디렉터리가 비어있어야 삭제가 가능하다.
+# os.unlink(파일)	    파일을 지운다.
+# os.rename(src, dst)	src라는 이름의 파일을 dst라는 이름으로 바꾼다.
+
+# glob - 디렉토리에 있는 파일들을 리스트로 만들어 주는 모듈
+dog_path = os.path.join(path, 'dog.*')
+len(glob(dog_path))
+
+dogs = []
+for dog_image in glob(dog_path):
+    dog = cv2.imread(dog_image)                           # 이미지 데이터를 읽기
+    # dog = cv2.cvtColor(dog, cv2.COLOR_BGR2GRAY)           # 흑백사진으로 변환
+    dog = cv2.resize(dog, (ROW, COL))                     # 가로,세로 사이즈 설정
+    # dog = image.img_to_array(dog)                         # 이미지를 array로 변환
+    # dog = np.transpose(dog, (2, 0, 1))
+    dogs.append(dog)
+len(dogs)
+
+##########################################################
+# 데이터 불러오기(cat_train)
+cat_path = os.path.join(path, 'cat.*')
+len(glob(cat_path))
+
+cats = []
+for cat_image in glob(cat_path):
+    cat = cv2.imread(cat_image)
+    # cat = cv2.cvtColor(cat, cv2.COLOR_BGR2GRAY)
+    cat = cv2.resize(cat, (ROW, COL))
+    # cat = image.img_to_array(cat)
+    # cat = np.transpose(cat, (2, 0, 1))
+    cats.append(cat)
+len(cats)
+
+##########################################################
+# enumerate - 인덱스 번호와 컬렉션의 원소를 tuple형태로 반환
+# dog=0, cat=1
+y_dog, y_cat = [], []
+
+y_dog = [0 for item in enumerate(dogs)]
+y_cat = [1 for item in enumerate(cats)]
+
+len(y_dog)
+len(y_cat)
+X = dogs + cats
+y = y_dog + y_cat
+len(X)
+len(y)
+
+
+##########################################################
+# 리스트의 형태를 ndarray로 바꿔줌
+dogs = mx.nd.array(dogs).astype('float32')
+cats = mx.nd.array(cats).astype('float32')
+
+y_dog = mx.nd.array(y_dog).astype('int32')
+y_cat = mx.nd.array(y_cat).astype('int32')
+
+##########################################################
+# 표준화를 하면 학습 속도가 더 빨라지고, 지역 최적의 상태에 빠지게 될 가능성을 줄여준다
+# local optima 요즘 trend에 의하면, 중요한 문제가 아니다(실제 딥러닝에서 local optima에 빠질 확률이 거의 없음)
+# values값을 0과 1 사이로 맞춰줌
+dogs /= 255                                                # 이미지는 숫자로 0~255의 8비트 부호없는 정수로 저장
+cats /= 255
+
+
+##########################################################
+# concatenate 함수를 이용해서 배열 결합
+X = mx.nd.concatenate([dogs, cats], axis=0)
+y = mx.nd.concatenate([y_dog, y_cat], axis=0)
+
+X = np.concatenate(dogs, cats, axis=0)
+y
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X,y, test_size=0.3)
+len(X_train)
+len(X_test)
+len(Y_train)
+len(Y_test)
+
+train_data = mx.io.NDArrayIter(data=X_train, label=Y_train)
+
+test_data = mx.io.NDArrayIter(data=X_test, label=Y_test)
+
+
+def transform_mnist(data, label):
+    # Transform a batch of examples.
+    resize = 224
+    if resize:
+        n = data.shape[0]
+        new_data = nd.zeros((n, resize, resize, data.shape[3]))
+        for i in range(n):
+            new_data[i] = mx.image.imresize(data[i], resize, resize)
+        data = new_data
+    # change data from batch x height x width x channel to batch x channel x height x width
+    return nd.transpose(data.astype('float32'),
+                        (0, 3, 1, 2)) / 255, label.astype('float32')
+
+
+
+# def transformer(data, label):
+#     new_data = nd.zeros((n, 224, 224, data.shape[3])
+#     for i in range(len(data)):
+#         new_data[i] = mx.image.imresize(data[i], 224, 224)
+#     data = new_data
+#
+#     return nd.transpose(data.astype('float32'), (2, 0, 1)) / 255, label.astype('float32')
+
+train_data = transform_mnist(X_train, Y_train)
+test_data = transform_mnist(X_test, Y_test)
+
+
+
 
 batch_size = 64
-train_data = gluon.data.DataLoader('./imageDataList_25.npy',
-    batch_size = batch_size, shuffle = False, last_batch = 'discard')
+train_data = mx.gluon.data.DataLoader(train_data, batch_size = batch_size, shuffle=False, last_batch = 'discard')
 
-test_data = gluon.data.DataLoader('./imageDataList_25.npy',
-    batch_size = batch_size, shuffle = True, last_batch = 'discard')
+test_data = mx.gluon.data.DataLoader(test_data, batch_size = batch_size, shuffle=False, last_batch = 'discard')
+
+
+
+for i, data in enumerate(train_data):
+
+
+
+
+
+
+
+
 
 
 
